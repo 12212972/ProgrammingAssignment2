5d89bd81a1cf421aab68755c7b6d3afb60ce092d
12212972 <khaki1225@nifty.ne.jp>
2015-08-23 22:03:27 +0900

    caching a matrix and its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## The "MakeCacheMatrix()" function saves a pair of input matrix and its inverse as a list named "cache".
+
+# The first one  saves a input matrix and its inverse pair in a list,
+# then second one returns the inverse matrix of the input by calculation 
+# or by calling from the cached data if possible.
+
 ## Write a short comment describing this function
+
+# input and its inverse matrix data is saved in a list named "cache".
+ 
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <<- list(x=x, y=solve(x))
 }
+
+
+
+
 
 
 ## Write a short comment describing this function
 
+# This function serches for the input matrix within cached inverse matrix data(cache$y).
+# The function returns its pair inverse matrix when the search succeeds.
+# The inverse is newly calculated if the search fails.
+
+
+
+###########################
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    flag <- TRUE
+    
+    if (!is.null(cache$y)){
+        for (i in length(cache$y)){
+            if(x[i] - cache$y[i] != 0) {
+                flag <- FALSE
+                break
+            }
+        }
+        print("getting cached data...")
+        return(cache$x)
+    }
+        
+    print("computing...")
+    return(solve(x))
+    
 }
+
+    
+
+
